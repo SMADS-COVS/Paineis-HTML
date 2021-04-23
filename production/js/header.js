a1a8d172d519e41e477825ef19e7b9453e1f0904
Victor de Souza Gomes Martins <x506821@rede.sp>
2021-04-23 11:33:59 -0300

    Subindo ultima versão paineis para o git

--- production/js/header.js
+++ production/js/header.js
@@ -1,7 +1,7 @@
 document.write(`
 <div class="col-md-3 left_col">
   <div class="left_col scroll-view">
-    <a href="home.html">
+    <a href="index.html">
       <div class="logo">
         <div class="img-responsive">
           <img alt="Prefeitura" src="images/logo-prefeitura.png" style="width: 100%">
@@ -18,49 +18,49 @@ document.write(`
           <li><a><i class="fa fa-home"></i> Acolhimento <span class="fa fa-chevron-down"></span></a>
             <ul class="nav child_menu">
               <li><a href="acolhimento-centvagas.html">Central de Vagas</a></li>
-              <li><a href="index2.html">Painel 2</a></li>
-              <li><a href="index3.html">Painel 3</a></li>
+              <li><a href="acolhimento-movimentacao-diaria.html">Movimentação Diária</a></li>
+              <li><a href="acolhimento-medida-protetiva.html">Medida Protetiva</a></li>
+              <li><a href="acolhimento-perfil-acolhido.html">Perfil do Acolhido</a></li>
             </ul>
           </li>
           <li><a><i class="fa fa-users"></i> Convivência <span class="fa fa-chevron-down"></span></a>
             <ul class="nav child_menu">
               <li><a href="convivencia-painel-cdcm.html">Painel CDCM</a></li>
-              <li><a href="form_advanced.html">Painel 2</a></li>
-              <li><a href="form_validation.html">Painel x</a></li>
-              <li><a href="form_wizards.html">Painel x</a></li>
-              <li><a href="form_upload.html">Painel x</a></li>
-              <li><a href="form_buttons.html">Painel x</a></li>
+              <li><a href="convivencia-atividades-cdcm.html">Atividades CDCM</a></li>
+              <li><a href="convivencia-atividades-camvv.html">Atividades CAMSV</a></li>
+			  <li><a href="convivencia-preench-form-mon.html">Preenchimento do Formulário de Monitoramento</a></li>
             </ul>
           </li>
           <li><a><i class="fa fa-signal"></i> Rede Direta <span class="fa fa-chevron-down"></span></a>
             <ul class="nav child_menu">
-              <li><a href="general_elements.html">Painel x</a></li>
-              <li><a href="media_gallery.html">Painel x</a></li>
-              <li><a href="typography.html">Painel x</a></li>
-              <li><a href="icons.html">Painel x</a></li>
+            <!--<li><a href="general_elements.html">Painel x</a></li>-->
+            <!--<li><a href="media_gallery.html">Painel x</a></li>-->
+            <!--<li><a href="typography.html">Painel x</a></li>-->
+            <!--<li><a href="icons.html">Painel x</a></li>-->
             </ul>
           </li>
           <li><a><i class="fa fa-male"></i> Perfil do Cidadão <span class="fa fa-chevron-down"></span></a>
             <ul class="nav child_menu">
-              <li><a href="tables.html">Painel x</a></li>
-              <li><a href="tables_dynamic.html">Painel x</a></li>
+            <!--<li><a href="tables.html">Painel x</a></li>-->
+            <!--<li><a href="tables_dynamic.html">Painel x</a></li>-->
             </ul>
           </li>
           <li><a><i class="fa fa-automobile"></i> Abordagem de Rua <span class="fa fa-chevron-down"></span></a>
             <ul class="nav child_menu">
-              <li><a href="chartjs.html">Painel x</a></li>
-              <li><a href="chartjs2.html">Painel x</a></li>
-              <li><a href="morisjs.html">Painel x</a></li>
-              <li><a href="echarts.html">Painel x</a></li>
-              <li><a href="other_charts.html">Painel x</a></li>
+            <!--  <li><a href="chartjs.html">Painel x</a></li>-->
+            <!--  <li><a href="chartjs2.html">Painel x</a></li>-->
+            <!--  <li><a href="morisjs.html">Painel x</a></li>-->
+            <!--  <li><a href="echarts.html">Painel x</a></li>-->
+            <!--  <li><a href="other_charts.html">Painel x</a></li>-->
             </ul>
           </li>
           <li><a><i class="fa fa-bar-chart"></i>Serviços <span class="fa fa-chevron-down"></span></a>
             <ul class="nav child_menu">
-              <li><a href="fixed_sidebar.html">Painel 3</a></li>
-              <li><a href="fixed_footer.html">Painel 3</a></li>
+            <!--  <li><a href="fixed_sidebar.html">Painel 3</a></li>-->
+            <!--  <li><a href="fixed_footer.html">Painel 3</a></li>-->
             </ul>
           </li>
+          <li><a href="dic-dados.html"><i class="fa fa-building-o"></i> Dicionário de Dados </a></li>
           <li><a href="sobre-nos.html"><i class="fa fa-building-o"></i> Sobre Nós </a></li>
           
         </ul>
